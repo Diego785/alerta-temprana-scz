326c9b6cbe64f40115efc3cb45613914ab993e17
melan <melanieyupanqui44@gmail.com>
2023-03-18 20:08:33 -0400

    unidad_alerta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             EventoSeeder::class,
+            UnidadTecnicoCientificaSeeder::class,
+            AlertaSeeder::class
             // EstadoSeeder::class,
             // AlertaSeeder::class,
         ]);
