fcd67dc994bef229fec6e655db8991fc1ed689e8
Diego785 <diegohurtado2412@gmail.com>
2023-06-07 11:39:39 -0400

    Melanie's changes

---
+++ app/Http/Livewire/Diagram/DiagramClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Diagram;
+
+use Livewire\Component;
+
+class DiagramClass extends Component
+{
+    public function render()
+    {
+        return view('livewire.diagram.diagram-class');
+    }
+}
